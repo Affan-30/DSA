0d2f6a42bbc64c871ff76f1d051c094a24b9bf77
Affan Shaikh <affansshaikh76@gmail.com>
2026-06-02 15:23:18 +0530

    Row sum calculation for a 2D array
    Implement a program to calculate the sum of each row in a 2D array and print the results.

---
+++ 2DarrayRowSum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        int[][] arr = {{1,2,3},{1,2,4},{1,2,5}};
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0 ; i<arr.length; i++){
+            int sum =0;
+             for(int j=0 ; j<arr[0].length; j++){
+            sum += arr[i][j];
+        }
+        list.add(sum);
+        }
+        
+        for(int i : list){
+            System.out.print(i+" ");
+        }
+    }
+}
