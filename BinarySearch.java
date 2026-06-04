728edbb337f6b9c006a363713409381869cb4103
Affan Shaikh <affansshaikh76@gmail.com>
2026-06-04 12:16:00 +0530

    Implement binary search algorithm

---
+++ BinarySearch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 1 && nums[0]==target)return 0;
+        int left = 0;
+        int right = nums.length-1;
+
+        int mid = (left + right)/2;
+        // More safe
+        // int mid = left + ((right-left)/2);
+
+        while(left <= right){
+        
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] < target){
+            left = mid+1;
+        }else{
+            right= mid-1;
+        }
+
+        mid = (left + right)/2;
+        }
+        
+        return -1;
+    }
+}
