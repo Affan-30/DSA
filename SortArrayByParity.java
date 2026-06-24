270a475e2b4b339e548e442a30db014da250bd99
Affan Shaikh <affansshaikh76@gmail.com>
2026-06-24 23:09:08 +0530

    Implement sorting of array by parity

---
+++ SortArrayByParity.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        if(nums.length  <= 1)return nums;
+
+        int s = 0;
+        int e = nums.length-1;
+
+        while(s <= e){
+            if(nums[s] % 2  != 0 && nums[e] % 2  == 0 ){
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }else if (nums[s] % 2  == 0 && nums[e] % 2  == 0){
+                s++;
+            }else if (nums[s] % 2  == 0 && nums[e] % 2  != 0){
+                s++;
+                e--;
+            }else if (nums[s] % 2  != 0 && nums[e] % 2  != 0){
+                e--;
+            }
+        }    
+        
+        return nums;
+    }
+}
