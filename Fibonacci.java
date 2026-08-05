637b06444b9bc7b6217a139afdc105dc99c11380
Affan Shaikh <affansshaikh76@gmail.com>
2026-08-05 22:46:48 +0530

    Implement Fibonacci sequence calculation

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,12 @@
 class Solution {
+    
     public int fib(int n) {
+        // Base Case
         if(n <= 1){
             return n;
         }
+
+        // recursion logic
         return fib(n-1) + fib(n-2);
     }
 }
